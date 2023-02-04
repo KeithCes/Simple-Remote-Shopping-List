b8a359664d444a1061f55a332237a332291116da
Keith Cestaro <34603915+KeithCes@users.noreply.github.com>
2023-02-04 14:48:26 -0500

    Created project, added Firebase SDK

---
+++ Simple-Remote-Shopping-List/Simple-Remote-Shopping-List/Simple_Remote_Shopping_ListApp.swift
@@ -0,0 +1,30 @@
+//
+//  Simple_Remote_Shopping_ListApp.swift
+//  Simple-Remote-Shopping-List
+//
+//  Created by Keith C on 2/4/23.
+//
+
+import SwiftUI
+import FirebaseCore
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+
+@main
+struct YourApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
